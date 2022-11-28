3efa61096d17db4bef76b7f78d12ba8d40db1e37
unknown <alfanwiguna@gmail.com>
2022-11-28 20:46:56 +0700

    update home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -42,7 +42,28 @@
                     </div>
                 </section>
                 <!-- Hero End -->
-
+                <section class="section feather-bg-img" style="background-image: url(images/features-bg-img-1.png)">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-lg-5">
+                                <p class="font-weight-medium text-uppercase mb-2"><i class="mdi mdi-chart-bubble h2 text-primary me-1 align-middle"></i> Creative Features</p>
+                                <h3 class="font-weight-semibold line-height-1_4 mb-4">SAMBUTAN KEPALA DINAS DLH KOTA SURAKARTA</h3>
+                                <!-- <h3 class="font-weight-semibold line-height-1_4 mb-4">Build <b>community</b> & <b>conversion</b> with our suite of <b>social tool</b></h3> -->
+                                <p class="text-muted font-size-15 mb-4 text-justify">UU Nomor 26 tahun 2007 tentang Penataan Ruang, ketentuan dan pengaturan 
+                                    ruang terbuka hijau menjadi bagian yang tidak terpisahkan dalam proses penataan ruang kawasan perkotaan. Setiap wilayah 
+                                    diwajibkan untuk mengalokasikan sedikitnya 30% dari ruang atau wilayahnya untuk RTH, dimana 20% diperuntukan bagi RTH publik yang merupakan ruang 
+                                    terbuka hijau yang dimiliki dan dikelola oleh pemerintah kota dan digunakan untuk kepentingan masyarakat secara umum, serta 10% diperuntukan bagi 
+                                    RTH private pada lahan-lahan yang dimiliki oleh swasta atau masyarakat.</p>
+                                
+                            </div>
+                            <div class="col-lg-6 offset-lg-1">
+                                <div class="mt-4 mt-lg-0">
+                                    <img src="{{ asset('img/kepaladinas.jpg') }}" alt="" class="img-fluid d-block mx-auto rounded-circle" width="600" height="600">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
 
         </div>
